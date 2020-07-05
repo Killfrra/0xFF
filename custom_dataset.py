3ad74b4b4dd9e379e4f8e741dfc13aa5ca295bc2
Killfrra <mak2001@ua.fm>
2020-07-05 04:13:39 +0300

    help and bugfixes

--- custom_dataset.py
+++ custom_dataset.py
@@ -6,12 +6,18 @@ from torch.utils.data import RandomSampler, SequentialSampler, BatchSampler
 import numpy as np
 
 class CustomBatchSampler(Sampler):
-    
+    """
+    Идея в том, чтобы при инициализации поделить места обитания индексов сэмплов на батчи,
+    а потом просто перетасовывать индексы и вырезать из них батчи
+
+    Индексы:    | 1 2 3 4 | 5 6 7 8 | 9 . . . |
+    Диапазоны:  0    -    4    -    8 - 9 (+ 3) 
+    батчей      |  batch  |  batch  |  batch  |
+    """
     def __init__(self, file, mem, num_replicas=1, rank=0, shuffle=True, drop_last=False):
-        #self.mem, self.shuffle, self.drop_last = (mem, shuffle, drop_last)
         self.epoch = 0
         self.indices = {}
-        self.slices = [] # all batches
+        self.batches = []
         self.num_batches = 0
         self.num_samples = 0
         for key in file:
@@ -26,29 +32,32 @@ class CustomBatchSampler(Sampler):
             for i in range(global_batch_count):
                 batch_start = i * global_batch_size + rank * batch_size
                 batch_end = batch_start + batch_size
-                self.slices.append((key, slice(batch_start, batch_end), 0))
+                self.batches.append((key, slice(batch_start, batch_end), 0))
             
             self.num_batches += global_batch_count
 
             remaining_samples = key_len - global_batch_count * global_batch_size
-            if not drop_last and remaining_samples > 0:
+            remaining_batch_size = 0
+            #if not drop_last and remaining_samples > 0:
+            if (not drop_last or remaining_samples % num_replicas == 0) and remaining_samples > 0:
                 # 1 < remaining_batch_size < batch_size
                 # remaining_batch_size * num_repicas >= key_len
-                remaining_batch_size = min(max(remaining_samples // num_replicas + (remaining_samples % num_replicas > 0), 1), batch_size)
+                remaining_batch_size = remaining_samples // num_replicas + (remaining_samples % num_replicas > 0)
                 batch_start = global_batch_count * global_batch_size + rank * remaining_batch_size
                 batch_end = batch_start + remaining_batch_size
                 
                 if batch_start < key_len and batch_end < key_len:
-                    self.slices.append((key, slice(batch_start, batch_end), 0))
+                    self.batches.append((key, slice(batch_start, batch_end), 0))
                 elif batch_start >= key_len:
-                    self.slices.append((key, slice(0), remaining_batch_size))
+                    self.batches.append((key, slice(0), remaining_batch_size))
                 elif batch_end >= key_len:
-                    self.slices.append((key, slice(batch_start, key_len), batch_end - key_len))
+                    self.batches.append((key, slice(batch_start, key_len), batch_end - key_len))
                 
                 self.num_batches += 1
+            #endif
 
             if drop_last:
-                key_len = global_batch_count * global_batch_size
+                key_len = global_batch_count * global_batch_size + num_replicas * remaining_batch_size
             self.indices[key] = range(key_len)
             self.num_samples += key_len        
 
@@ -58,9 +67,11 @@ class CustomBatchSampler(Sampler):
             self.indices[key] = torch.randperm(len(value), generator=g).tolist()
         batch_indices = torch.randperm(self.num_batches, generator=g).tolist()
         for batch in batch_indices:
-            key, _slice, additional_count = self.slices[batch]
-            additional = torch.randint(0, len(self.indices[key]), (additional_count, ), generator=g).tolist()
-            yield [ (key, index) for index in self.indices[key][_slice] ] + [ (key, index) for index in additional ]
+            key, _slice, additional_count = self.batches[batch]
+            res = [ (key, index) for index in self.indices[key][_slice] ]
+            if additional_count:
+                res += [ (key, index) for index in torch.randint(0, len(self.indices[key]), (additional_count, ), generator=g).tolist() ]
+            yield res
 
     def __len__(self):
         return self.num_batches
@@ -114,10 +125,11 @@ if __name__ == '__main__':
         }
     }
     
-    num_replicas = 4
+    #TODO: tests
+    num_replicas = 3
     for rank in range(num_replicas):
         print('rank', rank)
-        batch_sampler = CustomBatchSampler(file, 4, num_replicas, rank)
-        #print(batch_sampler.slices)
+        batch_sampler = CustomBatchSampler(file, 4, num_replicas, rank, drop_last=True)
+        #print(batch_sampler.batches)
         for batch in batch_sampler:
             print(batch)
